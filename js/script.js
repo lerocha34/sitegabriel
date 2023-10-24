c397cc96748b34d52a26310a8d675e93b5572d51
lerocha34 <le.vieirarocha@gmail.com>
2023-10-24 14:04:33 -0300

    enviando

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 let lerMais = document.querySelector(".article-area .article-texts #article1");
 let maisConteudo = 0;
-lerMais.addEventListener('click',()=>{
-    document.querySelector(".article-area .article-texts .article-text").style.height = "fit-content";
-    console.log('funcionou')
-    if (maisConteudo === 0){
-        lerMais.innerHTML = "Ler Menos"
-        maisConteudo = 1;
-    }
-})
+let texto = document.querySelector(".article-area .article-texts .article-text");
+lerMais.addEventListener('click',abrir)
+
+function abrir(){
+  if(maisConteudo === 0){
+    maisConteudo = 1;
+    texto.style.height = "auto";
+    lerMais.innerHTML = "Ler Manos";
+  } else {
+    maisConteudo = 0;
+    texto.style.height = "57px";
+    lerMais.innerHTML = "Ler Mais";
+  }
+}
+
