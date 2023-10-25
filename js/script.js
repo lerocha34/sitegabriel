7050f8c06e84095cd150034eecf3b22ccf4ffa37
lerocha34 <le.vieirarocha@gmail.com>
2023-10-25 11:21:25 -0300

    enviando versao 1.0.1

--- js/script.js
+++ js/script.js
@@ -21,38 +21,3 @@ lerMais.forEach(function(clique, index){
   })
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-let lerMais = document.querySelector(".article-area .article-texts #article1");
-let maisConteudo = 0;
-let texto = document.querySelector(".article-area .article-texts .article-text");
-lerMais.addEventListener('click',abrir)
-
-function abrir(){
-  if(maisConteudo === 0){
-    maisConteudo = 1;
-    texto.style.height = "auto";
-    lerMais.innerHTML = "Ler Manos";
-  } else {
-    maisConteudo = 0;
-    texto.style.height = "57px";
-    lerMais.innerHTML = "Ler Mais";
-  }
-}*/
