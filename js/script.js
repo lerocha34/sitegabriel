bebd91dc7fad75282e2f69385baf443bae0715a5
lerocha34 <le.vieirarocha@gmail.com>
2023-10-25 11:13:10 -0300

    enviando versão 1.0 da pagina

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+let lerMais = document.querySelectorAll(".article-area .article-texts .article-read");
+let maisConteudo = 0
+
+
+
+
+
+lerMais.forEach(function(clique, index){
+  clique.addEventListener("click", ()=>{
+    let texto = document.querySelectorAll(".article-text")[index];
+    if(maisConteudo === 0){
+      maisConteudo = 1;
+      texto.style.height = "auto";
+      clique.innerHTML = "Ler Menos";
+    } else {
+      maisConteudo = 0;
+      texto.style.height = "57px";
+      clique.innerHTML = "Ler Mais";}
+
+  
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let lerMais = document.querySelector(".article-area .article-texts #article1");
 let maisConteudo = 0;
 let texto = document.querySelector(".article-area .article-texts .article-text");
@@ -13,5 +55,4 @@ function abrir(){
     texto.style.height = "57px";
     lerMais.innerHTML = "Ler Mais";
   }
-}
-
+}*/
