f3856dc30c50d4c1d6d97f30669b3533ca705105
lerocha34 <le.vieirarocha@gmail.com>
2023-11-10 10:43:15 -0300

    enviando versao mobile

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 let openMenu = document.querySelector(".banner .banner-image .menu-buttons");
 let xButton = document.querySelector(".menu .menu-opened .close-menu");
 let menu = document.querySelector(".menu");
-
+let menuMobile = document.querySelector('.menu-buttons-mobile');
 
 function menuOpened(){
   menu.style.display = 'flex';
+  menuMobile.style.display ='none';
   mouse = 1;
 }
 function closeMenu(){
   menu.style.display = "none";
-
+  menuMobile.style.display ='flex';
 }
 
 function mouseVerify(event){
@@ -23,4 +24,5 @@ function mouseVerify(event){
 menu.addEventListener('mouseout', mouseVerify);
 openMenu.addEventListener('click',menuOpened);
 xButton.addEventListener('click',closeMenu);
+menuMobile.addEventListener('click',menuOpened);
 
