db92ce58766af96c98c42fa3ea5af9b2d8377ccb
lerocha34 <le.vieirarocha@gmail.com>
2023-11-10 16:54:39 -0300

    versao estilo dinner

--- js/script.js
+++ js/script.js
@@ -1,28 +1,37 @@
-let openMenu = document.querySelector(".banner .banner-image .menu-buttons");
-let xButton = document.querySelector(".menu .menu-opened .close-menu");
-let menu = document.querySelector(".menu");
-let menuMobile = document.querySelector('.menu-buttons-mobile');
+let menuBotao = document.querySelector('.menu-mobile-button');
+let closeMenu = document.querySelector('.close-menu');
 
-function menuOpened(){
-  menu.style.display = 'flex';
-  menuMobile.style.display ='none';
-  mouse = 1;
-}
-function closeMenu(){
-  menu.style.display = "none";
-  menuMobile.style.display ='flex';
-}
 
-function mouseVerify(event){
-  var relatedElement = event.relatedTarget;
-  if(!menu.contains(relatedElement)){
-    closeMenu()
+
+menuBotao.addEventListener("click", ()=>{
+    document.querySelector(".menu-mobile").style.display = 'flex';  
+    document.querySelector("body").style.overflowY = 'hidden';  
+    menuBotao.style.display = 'none';
+})
+
+
+closeMenu.addEventListener("click",()=>{
+    document.querySelector(".menu-mobile").style.display = 'none';
+    document.querySelector("body").style.overflowY = 'visible';
+    menuBotao.style.display = 'flex';
+
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  const gallery = document.querySelector(".gallery");
+  const items = document.querySelectorAll(".item");
+  const interval = 3000; // 3 segundos
+
+  let currentIndex = 0;
+
+  function nextSlide() {
+      currentIndex = (currentIndex + 1) % items.length;
+      updateGallery();
   }
-}
 
+  function updateGallery() {
+      gallery.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }
 
-menu.addEventListener('mouseout', mouseVerify);
-openMenu.addEventListener('click',menuOpened);
-xButton.addEventListener('click',closeMenu);
-menuMobile.addEventListener('click',menuOpened);
-
+  setInterval(nextSlide, interval);
+});
